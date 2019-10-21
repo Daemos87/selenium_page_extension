2579be101cc75542237fcb6b666f9574e97aa78f
VALUETEAM\GiangrandeAn <Antonio.GiangrandeDeRosa@nttdata.com>
2019-10-21 15:15:09 +0200

    first commit

---
+++ gian_selenium/decorators/decorators.py
@@ -0,0 +1,14 @@
+from gian_selenium.internals.types import locator
+
+
+def page(cls: type):
+    for key, value in filter(lambda x: not x[0].startswith('_')
+                                       and x[1] == locator,
+                             cls.__annotations__.items()):
+        setattr(cls, key, AutoWebElement(*cls.__dict__[key]))
+    old_init = cls.__init__
+    def _new_init(self, driver, *args, **kwargs):
+        self._driver = driver
+        old_init(self, *args, **kwargs)
+    cls.__init__ = _new_init
+    return cls
